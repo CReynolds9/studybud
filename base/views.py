339328343344cbf74791575abbf36fc59ae6288a
CReynolds9 <creyno35@charlotte.edu>
2025-02-05 20:23:52 -0500

    User Login

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from django.contrib.auth.models import User
+from .models import Room, Topic
 from .forms import RoomForm
 
 #rooms= [
@@ -8,14 +10,30 @@ from .forms import RoomForm
    # {'id' :3, 'name' : 'Frontend Developers'},
 #]
 
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 def home(request):
-    rooms = Room.objects.all()
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = Room.objects.get(id=pk)
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
